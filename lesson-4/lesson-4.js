d2be26510e25ef194d6a6849f9e639e977b9fbc4
Artur <golubev.artur.1986@gmail.com>
2023-01-19 10:55:34 +0300

    add: lesson topic: (Объекты)

---
+++ lesson-4/lesson-4.js
@@ -0,0 +1,31 @@
+// #4. Уроки по JavaScript для НАЧИНАЮЩИХ (Объекты)
+
+// Тип данных object
+// Объекты {}
+
+//const cart = {
+//	money: {
+//		sum: 500,
+//		currency: 'Руб',
+//		allowCurrency: ['Руб', 'Евро'],
+//		discount: {
+//			hasDiscount: true,
+//			value: 30,
+//		},
+//	},
+
+//	items: [{
+//		title: 'Ручку',
+//		count: 2,
+//		price: 100,
+//	}, {
+//		title: 'Книгу',
+//		count: 1,
+//		price: 300,
+//	}],
+//};
+
+//console.log(cart.money.sum);
+//console.log(cart.money.discount.value);
+
+//console.log(cart.items[1].title);
