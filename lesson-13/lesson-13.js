c2f1304fee4f5cabfac81e5fea8801937beb5059
Artur <golubev.artur.1986@gmail.com>
2023-01-19 14:29:44 +0300

    add: lesson topic: (конкатенация, innerHtml, textContent)

---
+++ lesson-13/lesson-13.js
@@ -0,0 +1,28 @@
+// #13. Уроки по JavaScript для НАЧИНАЮЩИХ (конкатенация, innerHtml, textContent)
+
+// Конкатенация
+// document без window
+// firstElementChild, lastElementChild, children
+// parentNode, nextElementSibling, previousElementSibling - sibling
+// innerHTML, textContent
+
+//const div = document.querySelector('div');
+
+//div.innerHTML = '<button>Hello world</button>';
+//div.textContent = '<button>Hello world</button>';
+
+//for (let i = 0; i < div.children.length; i++) {
+//	const element = div.children[i];
+
+//	if (element.tagName === 'SPAN') {
+//		console.log('SPAN ELEMENT');
+//	}
+//	console.log(element);
+//}
+
+//console.log(div.children);
+//console.log(div.firstElementChild);
+//console.log(div.lastElementChild);
+//console.log(div.parentNode);
+//console.log(div.nextElementSibling);
+//console.log(div.previousElementSibling); 
