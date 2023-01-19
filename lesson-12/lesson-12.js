dd241cf00f67b779e2ae568e639d9c0db5ac4441
Artur <golubev.artur.1986@gmail.com>
2023-01-19 14:26:56 +0300

    add: lesson topic: (window, querySelector, querySelectorAll)

---
+++ lesson-12/lesson-12.js
@@ -0,0 +1,23 @@
+// #12. Уроки по JavaScript для НАЧИНАЮЩИХ (window, querySelector, querySelectorAll)
+
+// window
+// getElementById
+// getElementsByClassName
+// getElementsByTagName
+// querySelector
+// querySelectorAll
+
+// console.log(window);
+
+//const button = window.document.getElementById('hello');
+//console.log(button);
+
+
+//const button = window.document.getElementsByClassName('test');
+//console.log(button);
+
+//const button = window.document.querySelector('.test > span');
+//console.log(button);
+
+//const buttons = window.document.querySelectorAll('.test > span');
+//console.log(buttons);
