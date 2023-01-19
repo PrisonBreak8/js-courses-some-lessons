9643e194a972a3afeeec335a75f9a05b8dd5b7ea
Artur <golubev.artur.1986@gmail.com>
2023-01-19 10:47:58 +0300

    add: lesson topic: (const, let, var, типы данных, typeof)

---
+++ lesson-2/lesson-2.js
@@ -0,0 +1,24 @@
+// #2. Уроки по JavaScript для НАЧИНАЮЩИХ (const, let, var, типы данных, typeof)
+
+// const, let, var
+// 
+// Типы данных
+// string: '' "" `` + экранирование
+// number: 5, 5.0, NaN, +-0, Infinity,
+// boolean: true/false
+// null
+// undefined
+// Symbol
+// BigInt
+//
+// Оператор typeof (typeof от typeof)
+// 
+// $_
+let helloWolrd = true;
+
+const type = "number";
+
+
+console.log(helloWolrd);
+console.log(typeof "number"); // выводим тип данных
+
