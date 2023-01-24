dcb42fbb383131af41a0a53bf3e760f674728af9
Artur <golubev.artur.1986@gmail.com>
2023-01-24 11:08:32 +0300

    add: GOODS array

---
+++ goods/goods.js
@@ -0,0 +1,13 @@
+const GOODS = [{
+	name: 'Уроки по HTML',
+	price: 300,
+	imgSrc: 'img/html.png',
+}, {
+	name: 'Уроки по CSS',
+	price: 150,
+	imgSrc: 'img/css.png',
+}, {
+	name: 'Уроки по JS',
+	price: 260,
+	imgSrc: 'img/js.png',
+}];
