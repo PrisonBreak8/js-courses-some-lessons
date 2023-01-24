d8513fc1b73a28f3a90b416b4c546cb41129c8d9
Artur <golubev.artur.1986@gmail.com>
2023-01-24 19:41:01 +0300

    fix: GOODS array, add id

--- goods/goods.js
+++ goods/goods.js
@@ -1,12 +1,15 @@
 const GOODS = [{
-	name: 'Уроки по HTML',
-	price: 300,
+	id: 1,
+	//name: 'Уроки по HTML',
+	//price: 300,
 	imgSrc: 'img/html.png',
 }, {
+	id: 2,
 	name: 'Уроки по CSS',
 	price: 150,
 	imgSrc: 'img/css.png',
 }, {
+	id: 3,
 	name: 'Уроки по JS',
 	price: 260,
 	imgSrc: 'img/js.png',
