4e015b76ac75f2f9d132040cd86bbdcabfca899d
Artur <golubev.artur.1986@gmail.com>
2023-01-26 18:16:41 +0300

    add: lesson topic: (Числа. NaN и Infinity, Math, toFixed, округляем)

---
+++ lesson-18/lesson-18.js
@@ -0,0 +1,63 @@
+// #18. Уроки по JavaScript для НАЧИНАЮЩИХ (Числа. NaN и Infinity, Math, toFixed, округляем)
+
+// Number
+// Math.pow / **
+// Math.ceil - к большему
+// Math.floor - к меньшему
+// Math.round - до ближайшего целого
+// Math.random
+// parseInt, parseFloat, toFixed
+// унарный +, Number, toString
+// isNaN, isInteger
+
+
+//========================================================================================================================================================
+
+
+// к большему
+//console.log(Math.ceil(0.1));
+//console.log(Math.ceil(0.5));
+//console.log(Math.ceil(0.9));
+
+
+// к меньшему
+//console.log(Math.floor(0.1));
+//console.log(Math.floor(0.5));
+//console.log(Math.floor(0.9));
+
+
+// до ближайшего целого
+//console.log(Math.round(0.1));
+//console.log(Math.round(0.5));
+//console.log(Math.round(0.9));
+
+
+
+//const test = 0.1 + 0.2;
+//console.log(test.toFixed(3));
+//console.log(test.toFixed(0));
+
+
+//console.log(Number('9.99'));
+//console.log(+('9.99'));
+//console.log(Number.parseFloat('9.99px'));
+//console.log(Number.parseInt('9.99px'));
+//const test = 10;
+//console.log(test.toString());
+
+
+//console.log(Math.pow(2, 5)); // в степень возвести число
+//console.log(2 ** 5); // в степень второй вариант
+
+
+
+//const test = 123 / 'test';
+//console.log(Number.isNaN(test));
+//console.log(Number.isInteger(test));
+
+
+
+//console.log(Math.random());
+//console.log(Math.random() * 2);
+//console.log(Math.random() * 2 + 1);
+//console.log(Math.floor(Math.random() * 10 + 1));
